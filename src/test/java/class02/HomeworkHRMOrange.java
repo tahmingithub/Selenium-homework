de7fbf47b7e1777ba584fac14692aee4a683e35c
tahmingithub <tahminnka@gmail.com>
2023-09-26 17:05:36 -0400

    Selenuim homework

--- src/test/java/class02/HomeworkHRMOrange.java
+++ src/test/java/class02/HomeworkHRMOrange.java
@@ -10,19 +10,29 @@ public class HomeworkHRMOrange {
     public static void main(String[] args) throws InterruptedException {
 
       WebDriver driver= new ChromeDriver();
+      //maximize the page
       driver.manage().window().maximize();
+      //open the OrangeHrm login page
       driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+      // sleep time to 3 sec
       Thread.sleep(3000);
+      //enter the username as "abc"
         WebElement username= driver.findElement(By.name("username"));
         username.sendKeys("abc");
         username.clear();
+        // //enter the username as "admin"
         username.sendKeys("Admin");
+        // enter the pass as admin123
         WebElement pass= driver.findElement(By.name("password"));
         pass.sendKeys("admin123");
+        //get text from login button
         WebElement login= driver.findElement(By.tagName("button"));
         String text= login.getText();
+        //print it on the console
         System.out.println(text);
+        //click login
         login.click();
+        //quit the whole browser
         driver.quit();
 
 
